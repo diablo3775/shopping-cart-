63f47e22b0621a4efc57cb9b4b3ea2ab80d41c93
Anil Kumar <72590342+diablo3775@users.noreply.github.com>
2022-11-15 15:56:24 +0530

    Update products.js

--- shopping cart/products.js
+++ shopping cart/products.js
@@ -2,7 +2,7 @@ const products = [
   {
     id: 0,
     name: "T-shirt 1",
-    price: 29.99,
+    price: 299.99,
     instock: 100,
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
@@ -11,7 +11,7 @@ const products = [
   {
     id: 1,
     name: "T-shirt 2",
-    price: 24.99,
+    price: 244.99,
     instock: 43,
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
@@ -20,7 +20,7 @@ const products = [
   {
     id: 2,
     name: "T-shirt 3",
-    price: 19.99,
+    price: 199.99,
     instock: 10,
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
@@ -29,7 +29,7 @@ const products = [
   {
     id: 3,
     name: "T-shirt 4",
-    price: 25.99,
+    price: 255.99,
     instock: 5,
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
@@ -38,7 +38,7 @@ const products = [
   {
     id: 4,
     name: "T-shirt 5",
-    price: 29.99,
+    price: 299.99,
     instock: 4,
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
@@ -47,7 +47,7 @@ const products = [
   {
     id: 5,
     name: "T-shirt 6",
-    price: 39.99,
+    price: 399.99,
     instock: 40,
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
